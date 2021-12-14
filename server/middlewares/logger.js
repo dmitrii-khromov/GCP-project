53aef253524f9c53e2b9601fed3f79c0cb1ff9e8
dmitrii-khromov <atomo90@mail.ru>
2021-12-14 20:21:06 +0300

    Switch logging to a console.

--- server/middlewares/logger.js
+++ server/middlewares/logger.js
@@ -1,17 +1,21 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
-const path = require('path');
+//const path = require('path');
 
 const requestLogger = expressWinston.logger({
+  level: 'info',
   transports: [
-    new winston.transports.File({ filename: path.join('logs', 'request.log') }),
+    new winston.transports.Console(),
+    // new winston.transports.File({ filename: path.join('logs', 'request.log') }),
   ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
+  level: 'error',
   transports: [
-    new winston.transports.File({ filename: path.join('logs', 'error.log') }),
+    new winston.transports.Console(),
+    // new winston.transports.File({ filename: path.join('logs', 'error.log') }),
   ],
   format: winston.format.json(),
 });
